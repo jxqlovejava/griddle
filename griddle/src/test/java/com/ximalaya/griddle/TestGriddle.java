216add8a001f1a6faffff57529309aad792a64a4
jxqlovejava <jxqlovejava@163.com>
2014-05-10 02:20:17 +0800

    move LINCENCE file to global

--- griddle/src/test/java/com/ximalaya/griddle/TestGriddle.java
+++ griddle/src/test/java/com/ximalaya/griddle/TestGriddle.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.util.Assert;
 
 @ContextConfiguration(locations="classpath:application-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
